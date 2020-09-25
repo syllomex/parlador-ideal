be97e753ca417fae359797597732a93ec2b1e173
syllomex <leonardojdossantos@gmail.com>
2020-09-25 13:08:13 -0300

    chore: add and configure font family inter

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,24 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React from "react";
 
 import { StackNavigator } from "./src/navigators/stack";
 
+import {
+  useFonts,
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
+
 export default function App() {
-  const [state, setState] = useState({ appIsReady: false });
+  let [fontsLoaded] = useFonts({
+    Inter_300Light,
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) return null;
 
   return (
     <React.Fragment>
