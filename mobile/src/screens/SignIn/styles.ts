ff5749d5551d53e02c2a16aee09100dbc3025b01
syllomex <leonardojdossantos@gmail.com>
2020-09-26 14:30:51 -0300

    fix: corrects buttons alignment

--- mobile/src/screens/SignIn/styles.ts
+++ mobile/src/screens/SignIn/styles.ts
@@ -3,6 +3,7 @@ import styled from "styled-components/native";
 export const ActionsContainer = styled.View`
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
 
   width: 100%;
   margin: 48px 0;
--- mobile/src/screens/SignUp/styles.ts
+++ mobile/src/screens/SignUp/styles.ts
@@ -8,6 +8,7 @@ export const TitleContainer = styled.View`
 export const ActionsContainer = styled.View`
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
 
   width: 100%;
   margin: 48px 0;
