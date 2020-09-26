4cb25e6607b4bbbba62d28a5c902dd33d05b3a12
syllomex <leonardojdossantos@gmail.com>
2020-09-25 22:34:31 -0300

    feat: add new colors

--- mobile/src/assets/color.ts
+++ mobile/src/assets/color.ts
@@ -1,6 +1,13 @@
 export const color = {
   primary: "#93ECAD",
-  secondary: "#D2D2D2",
-  textInPrimary: "#222",
-  textMuted: "#777",
+  background: "#FEFEFE",
+  gray_background: "#E2E2E2",
+  dark_gray: "#5C5C5C",
+  light_gray: "#D4D4D4",
+  text_muted: "#898989",
+  dark: "#222222",
+  light: "#FFF",
+  placeholder_in_primary: "#555555",
+  link: "#6FA8FF",
+  line: "#767676",
 };
