7940a0c8f566aedc3e3d9d8743476531d3cd05e6
syllomex <leonardojdossantos@gmail.com>
2020-09-26 11:49:50 -0300

    feat: add fixed header option to wrapper component

--- mobile/src/components/Wrapper/index.tsx
+++ mobile/src/components/Wrapper/index.tsx
@@ -1,21 +1,31 @@
 import React from "react";
+import { ScrollViewProps, StyleSheet } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { color } from "../../assets/color";
 
-const Wrapper: React.FC = ({ children }) => {
+interface IProps extends ScrollViewProps {
+  fixedHeader?: React.ReactNode;
+}
+
+const Wrapper: React.FC<IProps> = ({ fixedHeader, children }) => {
   return (
-    <ScrollView
-      contentContainerStyle={{
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: color.background,
-        minHeight: "100%",
-        paddingHorizontal: "10%",
-      }}
-    >
-      {children}
-    </ScrollView>
+    <React.Fragment>
+      {fixedHeader && fixedHeader}
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        {children}
+      </ScrollView>
+    </React.Fragment>
   );
 };
+
+const styles = StyleSheet.create({
+  scrollView: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: color.background,
+    minHeight: "100%",
+    paddingHorizontal: "10%",
+  },
+});
 
 export default Wrapper;
