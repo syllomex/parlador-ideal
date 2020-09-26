d260fea6e1a4b1ea5f692191c9fea980a8f17418
syllomex <leonardojdossantos@gmail.com>
2020-09-26 10:32:07 -0300

    feat: add icon option to title component

--- mobile/src/components/Title/index.tsx
+++ mobile/src/components/Title/index.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { TextProps } from "react-native";
-import { Text } from "./styles";
 
-const Title: React.FC<TextProps> = ({ children, ...props }) => {
+import { Container, Icon, Text } from "./styles";
+
+interface IProps extends TextProps {
+  withIcon?: boolean;
+}
+
+const Title: React.FC<IProps> = ({ withIcon, children, ...props }) => {
+  if (withIcon)
+    return (
+      <Container>
+        <Icon />
+        <Text {...props}>{children}</Text>
+      </Container>
+    );
+
   return <Text {...props}>{children}</Text>;
 };
 
--- mobile/src/components/Title/styles.ts
+++ mobile/src/components/Title/styles.ts
@@ -1,8 +1,19 @@
 import styled from "styled-components/native";
 import { color } from "../../assets/color";
 
+import logo_small from "../../assets/images/icons/logo_small.png";
+
 export const Text = styled.Text`
   font-family: "Inter_400Regular";
   font-size: 22px;
   color: ${color.primary};
+`;
+
+export const Container = styled.View`
+  flex-direction: row;
+  align-items: flex-start;
+`;
+
+export const Icon = styled.Image.attrs({ source: logo_small })`
+  margin-right: 4px;
 `;
