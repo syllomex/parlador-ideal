1f8e9aadd367988e901e168c3a73660f25b74a7e
syllomex <leonardojdossantos@gmail.com>
2020-09-28 16:24:19 -0300

    test: ensure integration of post entity and mongodb model

---
+++ server/src/tests/integration/post.spec.ts
@@ -0,0 +1,82 @@
+import { connect, disconnect } from "../../database";
+import { Users } from "../../database/models/Users";
+import { Post } from "../../entities/Post";
+import { User } from "../../entities/User";
+import { PostMongoRepository } from "../../repositories/implementations/PostMongoRepository";
+import { UserMongoRepository } from "../../repositories/implementations/UserMongoRepository";
+
+describe("Name of the group", () => {
+  it("should connect to mongo cloud database", async () => {
+    const response = await connect();
+    expect(response).toBeUndefined();
+  });
+
+  const userRepo = new UserMongoRepository();
+  const postRepo = new PostMongoRepository();
+
+  // it is necessary to create a user before testing posts
+  const user = new User({
+    name: "Any Name",
+    email: "any_email@mail.com",
+    password: "any_password",
+  });
+
+  const post = new Post({
+    content: "any content",
+    date: new Date(),
+    user: user.id,
+  });
+
+  it("create a user for posts tests", async () => {
+    await userRepo.create(user);
+  });
+
+  it("check if a post instance was created", async () => {
+    expect(post).toHaveProperty("id");
+    expect(post).toHaveProperty("content");
+    expect(post).toHaveProperty("date");
+    expect(post).toHaveProperty("user");
+    expect(post.user).toEqual(user.id);
+  });
+
+  it("should insert post on database returning the created post", async () => {
+    const created_post = await postRepo.create(post);
+
+    expect(created_post).toEqual({
+      id: post.id,
+      content: post.content,
+      date: post.date,
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+      },
+    });
+  });
+
+  it("should update created post content", async () => {
+    const new_content = "any new content";
+    const updated_post = await postRepo.update(new_content, post.id);
+
+    expect(updated_post).toEqual({
+      id: post.id,
+      content: new_content,
+      date: post.date,
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+      },
+    });
+  });
+
+  it("should delete created post", async () => {
+    await postRepo.remove(post.id);
+  });
+
+  it("delete created user", async () => {
+    await Users.findByIdAndRemove(user.id);
+  });
+
+  disconnect();
+});
