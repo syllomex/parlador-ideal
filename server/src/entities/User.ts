6f5c3df7945975728a54cb8cc325f02005a33855
syllomex <leonardojdossantos@gmail.com>
2020-09-28 13:50:14 -0300

    feat: add user entity

---
+++ server/src/entities/User.ts
@@ -0,0 +1,16 @@
+import { v4 as uuid } from "uuid";
+
+class User {
+  public readonly id!: string;
+  public name!: string;
+  public email!: string;
+  public password!: string;
+
+  constructor(props: Omit<User, "id">, id?: string) {
+    Object.assign(this, props);
+
+    if (!id) this.id = uuid();
+  }
+}
+
+export { User };
