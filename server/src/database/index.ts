e71880d7a9edca9bde98241128a7fffb5553ecde
syllomex <leonardojdossantos@gmail.com>
2020-09-29 10:15:13 -0300

    refactor: change mongo connect code

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,23 +1,20 @@
 import mongoose from "mongoose";
 
+const mongo_options: mongoose.ConnectionOptions = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+};
+
 async function connect(): Promise<void> {
-  try {
-    let MONGO_URI: string | undefined;
-    MONGO_URI = process.env.MONGO_URI;
+  let MONGO_URI: string | undefined;
+  MONGO_URI = process.env.MONGO_URI;
 
-    if (!MONGO_URI) throw new Error("missing mongo uri environment variable");
+  if (!MONGO_URI) throw new Error("missing MONGO_URI environment variable");
 
-    await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    });
-
-    console.log("connected to mongodb");
-  } catch (error) {
-    console.error(`can not connect to mongodb: ${error.message}`);
-  }
+  await mongoose.connect(MONGO_URI, mongo_options);
+  console.log("connected to mongodb");
 }
 
 async function disconnect() {
