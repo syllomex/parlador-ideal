7a882201d145c101b5ce2fe8b342bf90e690723f
syllomex <leonardojdossantos@gmail.com>
2020-09-29 20:23:16 -0300

    feat: add payload to http request protocol

--- server/src/protocols/http.ts
+++ server/src/protocols/http.ts
@@ -1,6 +1,9 @@
+import { IPayload } from "../utils/access-token/payload";
+
 export interface HttpRequest {
   body?: any;
   headers?: any;
+  payload: IPayload;
 }
 
 export interface HttpResponse {
