579612e442c3743afbcbedf6dd276bf3ba1d8639
syllomex <leonardojdossantos@gmail.com>
2020-09-29 00:42:04 -0300

    test: add coverage to undefined node env on mongo connection

--- server/src/tests/integration/database.spec.ts
+++ server/src/tests/integration/database.spec.ts
@@ -2,6 +2,7 @@ import { connect, disconnect } from "../../database";
 
 describe("Test mongodb connection", () => {
   it("should connect to mongo cloud database", async () => {
+    process.env.NODE_ENV = "development";
     const response = await connect();
     expect(response).toBeUndefined();
   });
@@ -9,5 +10,12 @@ describe("Test mongodb connection", () => {
   it("should disconnect from mongo cloud database", async () => {
     const response = await disconnect();
     expect(response).toBeUndefined();
+  });
+
+  it("should thrown when mongo uri is undefined", async () => {
+    process.env.NODE_ENV = undefined;
+    expect(async () => {
+      connect();
+    }).rejects.toThrow();
   });
 });
