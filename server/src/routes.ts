9ab8f312e7b264b9d51bf043f7e29293a5b0c1df
syllomex <leonardojdossantos@gmail.com>
2020-09-29 17:48:19 -0300

    feat: add sign in route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,12 +3,14 @@ const router = Router();
 
 import { IController } from "./protocols/controller";
 import { createUserController } from "./use-cases/CreateUser";
+import { signInController } from "./use-cases/SignIn";
 
 async function sendRes(controller: IController, req: Request, res: Response) {
   const { code, body } = await controller.handle(req);
   return res.status(code).json(body);
 }
 
+router.post("/auth", (req, res) => sendRes(signInController, req, res));
 router.post("/users", (req, res) => sendRes(createUserController, req, res));
 
 export { router };
