c799387d70058bdd8d2c6f225ebb580488ade86c
syllomex <leonardojdossantos@gmail.com>
2020-09-30 10:50:42 -0300

    feat: submit sign in data to api

--- mobile/src/screens/SignIn/index.tsx
+++ mobile/src/screens/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Image } from "react-native";
 import Button from "../../components/Button";
 import InputBlock from "../../components/InputBlock";
@@ -10,14 +10,33 @@ import { ActionsContainer } from "./styles";
 import logo_large from "../../assets/images/icons/logo_large.png";
 import Wrapper from "../../components/Wrapper";
 import { useNavigation } from "@react-navigation/native";
+import { api } from "../../services/api";
+import { useProfile } from "../../contexts/profile";
 
 const SignIn: React.FC = () => {
+  const { profile, setProfile } = useProfile();
+
   const [fields, setFields] = useState({
     email: "",
     password: "",
   });
 
   const { navigate } = useNavigation();
+
+  async function handleSubmit() {
+    try {
+      if (fields.email === "" || fields.password === "") return;
+
+      const response = await api.post("/auth", fields);
+      setProfile(response.data);
+    } catch (error) {
+      console.log(error.response.status, error.response.data.message);
+    }
+  }
+
+  useEffect(() => {
+    if (profile) navigate("Posts");
+  }, [profile]);
 
   return (
     <Wrapper>
@@ -45,7 +64,7 @@ const SignIn: React.FC = () => {
 
       <ActionsContainer>
         <Link onPress={() => navigate("SignUp")}>Cadastrar-se</Link>
-        <Button onPress={() => navigate("Posts")}>Entrar</Button>
+        <Button onPress={handleSubmit}>Entrar</Button>
       </ActionsContainer>
 
       <Image source={logo_large} />
