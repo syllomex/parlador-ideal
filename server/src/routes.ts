01871bd3a4784901a0461e05e67cf468db627eef
syllomex <leonardojdossantos@gmail.com>
2020-09-29 21:26:33 -0300

    feat: add remove post route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,15 @@
 import { Request, Response, Router } from "express";
 import { isLogged } from "./middlewares/isLogged";
+
 const router = Router();
 
 import { IController } from "./protocols/controller";
+
+import { signInController } from "./use-cases/SignIn";
 import { createPostController } from "./use-cases/CreatePost";
 import { createUserController } from "./use-cases/CreateUser";
 import { indexPostsController } from "./use-cases/IndexPosts";
-import { signInController } from "./use-cases/SignIn";
+import { removePostController } from "./use-cases/RemovePost";
 
 async function sendRes(controller: IController, req: Request, res: Response) {
   const { code, body } = await controller.handle(req);
@@ -18,5 +21,6 @@ router.post("/users", (req, res) => sendRes(createUserController, req, res));
 
 router.get("/posts", isLogged, (req, res) => sendRes(indexPostsController, req, res));
 router.post("/posts", isLogged, (req, res) => sendRes(createPostController, req, res));
+router.delete("/posts/:id", isLogged, (req, res) => sendRes(removePostController, req, res));
 
 export { router };
