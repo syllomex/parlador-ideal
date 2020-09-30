2e9cee1e20a802db4bb9f20888c8b177b9067c96
syllomex <leonardojdossantos@gmail.com>
2020-09-30 10:48:30 -0300

    feat: add routes control

--- mobile/src/navigators/stack.tsx
+++ mobile/src/navigators/stack.tsx
@@ -6,37 +6,46 @@ import SignUp from "../screens/SignUp";
 import Posts from "../screens/Posts";
 import NewPost from "../screens/NewPost";
 import EditPost from "../screens/EditPost";
+import { useProfile } from "../contexts/profile";
 
 const Stack = createStackNavigator();
 
 export const StackNavigator: React.FC = () => {
-  return (
-    <Stack.Navigator initialRouteName="SignIn">
-      <Stack.Screen
-        name="SignIn"
-        component={SignIn}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="SignUp"
-        component={SignUp}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="Posts"
-        component={Posts}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="NewPost"
-        component={NewPost}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="EditPost"
-        component={EditPost}
-        options={{ headerShown: false }}
-      />
-    </Stack.Navigator>
-  );
+  const { profile } = useProfile();
+
+  if (profile)
+    return (
+      <Stack.Navigator initialRouteName="Posts">
+        <Stack.Screen
+          name="Posts"
+          component={Posts}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="NewPost"
+          component={NewPost}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="EditPost"
+          component={EditPost}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    );
+  else
+    return (
+      <Stack.Navigator initialRouteName="SignIn">
+        <Stack.Screen
+          name="SignIn"
+          component={SignIn}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    );
 };
